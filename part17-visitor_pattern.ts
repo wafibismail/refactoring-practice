85672767f26504507547eefce255e4cd097bf2a5
Abdul Wafi Haji Ismail <wafibismail@gmail.com>
2022-04-04 10:56:15 +0800

    Update Part17*.ts

---
+++ part17-visitor_pattern.ts
@@ -0,0 +1,10 @@
+/*
+    An exercise/problem:
+        - A company needs a program that calculates bonuses for each of its employees
+        - Calculate quarterly and year-end bonuses
+        - Each employee receives a bonus based off of different criteria
+        - Use the visitor pattern
+        - Employee classes can only be added to, and nothing can be changed.
+
+    [Continue from https://youtu.be/4tLSDAbe8QY?t=267 (minute 4:27)]
+*/
